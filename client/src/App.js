198e6e12f506c24959b89220f1250e16d1a59d51
Bug Mika <bugtmika@gmail.com>
2019-04-03 11:28:35 -0400

    fixed mobile

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import Content from './components/Content.js';
-import Nav from './components/Nav.js';
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <div className='desktop'>
-          <Nav />
-        </div>
         <Content />
-        <div className='mobile'>
-          <Nav />
-        </div>
       </div>
     );
   }
